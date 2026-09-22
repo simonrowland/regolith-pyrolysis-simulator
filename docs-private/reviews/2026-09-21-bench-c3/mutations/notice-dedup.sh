28546a466cb91975996e9cda29edb22cc1452cfb
Simon Rowland <simon@simonrowland.com>
2026-09-22 17:27:35 -0400

    chore: stop tracking private review notes that bypassed the ignore rule

--- docs-private/reviews/2026-09-21-bench-c3/mutations/notice-dedup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-ROOT=$(git rev-parse --show-toplevel)
-cd "$ROOT"
-PYTHON=${PYTHON:-python3}
-exec "$PYTHON" docs-private/reviews/2026-09-21-bench-c3/mutations/run_guard.py notice-dedup
